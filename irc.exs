3433ecfe3dfa4d719858066d745dbb2220e4541d
= <Phi@gold.ratio>
2016-09-20 17:57:06 +0000

    Commited first Elixir IRC Bot Source

---
+++ irc.exs
@@ -0,0 +1,45 @@
+#Test IRC Connection Application written by Intangere
+
+defmodule Irc do
+  use Application
+  
+  def connect(host, port) do
+    {:ok, socket} = :gen_tcp.connect(host, port, [:binary, active: false])
+    sendAll(socket, "NICK Test_account")
+    sendAll(socket, "USER Test_account Test_account Test_account :Test_account")
+    IO.puts("Connected to #{host}.")
+    loop(socket)
+  end
+
+  def sendAll(socket, data) do
+    :gen_tcp.send(socket, data <> "\r\n")
+    IO.puts("Sent")
+  end
+
+  def recv(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    IO.puts(data)
+    data
+  end
+
+  def loop(socket) do 
+    connected = true
+    if connected == true do
+      data = recv(socket)
+      if data != "" do
+        if String.slice(data, 0..4) == "PING" do
+          pong = "PONG " <> List.first(String.split(data[1], " "))
+          IO.puts(pong)
+          sendAll(socket, pong)
+        end
+        loop(socket)
+      else
+        connected = false
+      end
+    else
+      IO.puts("Connection to server terminated.") 
+    end
+  end
+end
+
+Irc.connect('irc.freenode.net', 6667)
